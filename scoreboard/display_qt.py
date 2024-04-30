d6c2d6d7a049876990ea4597975229146f837a11
Craig DeHaan <craig.s.dehaan@gmail.com>
2024-04-30 13:49:34 -0400

    QT display refactor

--- scoreboard/display_qt.py
+++ scoreboard/display_qt.py
@@ -4,17 +4,15 @@ from .splash_screen import SplashScreen
 
 LED_SIZE=8
 
-class RgbLed(Qt.QWidget):
+class RgbLed:
 
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setMinimumSize(5,5)
-        self.setMaximumSize(300,300)
-        self.setSizePolicy(Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Expanding)
+    def __init__(self, x, y, size=LED_SIZE):
+        self.x = x
+        self.y = y
+        self.setSize(size)
         self.off()
 
-    def paintEvent(self, event):
-        painter = Qt.QPainter(self)
+    def paint(self, painter):
         painter.setRenderHint(Qt.QPainter.Antialiasing, True)
         rect = self.rect().adjusted(1,1,-1,-1)
         hilite = Qt.QPoint(int(rect.width()/2), int(rect.height()/4))
@@ -24,37 +22,56 @@ class RgbLed(Qt.QWidget):
         painter.setBrush(Qt.QBrush(gradient))
         painter.drawEllipse(rect)
 
+    def rect(self):
+        return Qt.QRect(self.x, self.y, self.size, self.size)
+
     def setSize(self, size):
-        self.setFixedSize(size, size)
+        self.size = size
 
     def on(self, color):
         self.color = color
-        self.update()
 
     def off(self):
         self.on(Qt.QColor('black'))
 
 
-class Display(Qt.QApplication):
-
-    def __init__(self, config):
-        super().__init__([])
-        self.rows = config.screen_rows()
-        self.cols = config.screen_cols()
-        self.win = Qt.QMainWindow()
-        self.win.resize(self.cols*LED_SIZE, self.rows*LED_SIZE)
-        self.win.move(config.display.getint("window_x", 100), config.display.getint("window_y", 100))
-        self.win.setStyleSheet('background-color: black;')
-        self.win.setAutoFillBackground( True )
+class LedDisplay(Qt.QMainWindow):
+    def __init__(self, rows, cols):
+        super().__init__()
+        self.rows = rows
+        self.cols = cols
 
         # leds
         self.leds = []
         for row in range(self.rows):
             self.leds.append([])
             for col in range(self.cols):
-                self.leds[row].append(RgbLed(self.win))
-                self.leds[row][col].setSize(LED_SIZE)
-                self.leds[row][col].move(col*LED_SIZE, row*LED_SIZE)
+                self.leds[row].append(RgbLed(col*LED_SIZE, row*LED_SIZE))
+
+
+    def show_img(self, image):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                pixel = image.getpixel((col,row))
+                self.leds[row][col].on(Qt.QColor(pixel[0], pixel[1], pixel[2]))
+
+
+    def paintEvent(self, event):
+        painter = Qt.QPainter(self)
+        for row in range(self.rows):
+            for col in range(self.cols):
+                self.leds[row][col].paint(painter)
+
+
+class Display(Qt.QApplication):
+
+    def __init__(self, config):
+        super().__init__([])
+        self.win = LedDisplay(config.screen_rows(), config.screen_cols())
+        self.win.resize(config.screen_cols()*LED_SIZE, config.screen_rows()*LED_SIZE)
+        self.win.move(config.display.getint("window_x", 100), config.display.getint("window_y", 100))
+        self.win.setStyleSheet('background-color: black;')
+        self.win.setAutoFillBackground( True )
 
         self.splash = SplashScreen('splash', config)
         self.show_splash("Starting")
@@ -66,10 +83,8 @@ class Display(Qt.QApplication):
 
 
     def show(self, image):
-        for row in range(self.rows):
-            for col in range(self.cols):
-                pixel = image.getpixel((col,row))
-                self.leds[row][col].on(Qt.QColor(pixel[0], pixel[1], pixel[2]))
+        self.win.show_img(image)
+        self.win.update()
 
 
     def show_splash(self, msg):
